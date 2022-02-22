c9a07ca826bee07d15a7bc5c81b41afb42581f70
AgentRediska <deniahalk98@mail.ru>
2022-02-22 12:33:08 +0300

    made activity layout

--- app/src/main/java/com/agentrediska/movieinfo/presentation/MainActivity.kt
+++ app/src/main/java/com/agentrediska/movieinfo/presentation/MainActivity.kt
@@ -3,10 +3,15 @@ package com.agentrediska.movieinfo.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.agentrediska.movieinfo.R
+import com.agentrediska.movieinfo.presentation.screen.film.ListFilmsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, ListFilmsFragment())
+            .commit()
     }
 }
